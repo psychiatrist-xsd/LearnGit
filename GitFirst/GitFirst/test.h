c1788e29127659d83b9290b36f399bcb9182e7f7
psychiatristt1 <995366159@qq.com>
2020-10-26 19:26:23 +0800

    第二次提交

---
+++ GitFirst/GitFirst/test.h
@@ -0,0 +1,17 @@
+//
+//  test.h
+//  GitFirst
+//
+//  Created by 许世盾 on 2020/10/26.
+//  Copyright © 2020 许世盾. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface test : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitFirst/GitFirst/test.m
@@ -0,0 +1,21 @@
+//
+//  test.m
+//  GitFirst
+//
+//  Created by 许世盾 on 2020/10/26.
+//  Copyright © 2020 许世盾. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
